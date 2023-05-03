13227803b545f4edf96fc463ce73d65f4f68c243
Lord_Redst0ne <74739340+enemieltje@users.noreply.github.com>
2023-05-03 13:42:40 +0200

    added anti aliasing

--- src/client/GLTFViewer.js
+++ src/client/GLTFViewer.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { SSAARenderPass } from 'three/addons/postprocessing/SSAARenderPass.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 export default class GLTFViewer {
 
@@ -11,26 +14,37 @@ export default class GLTFViewer {
     static controls
     static gltfObject
     static light
+    static gui
+    static ssaaRenderPass
+    static composer
 
     /**
      * creates a scene that can display gltf/glb files
      */
     static start() {
+        const width = window.innerWidth - 50
+        const height = window.innerHeight - 50
+        const aspectRatio = width / height
+        const scale = 0.15;
 
         // create new renderers from the THREE engine
         GLTFViewer.scene = new THREE.Scene();
-        GLTFViewer.camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
+        // GLTFViewer.camera = new THREE.PerspectiveCamera(20, aspectRatio, 0.1, 1000);
+        GLTFViewer.camera = new THREE.OrthographicCamera(-aspectRatio * scale, aspectRatio * scale, scale, -scale, 0.1, 1000);
+
         GLTFViewer.renderer = new THREE.WebGLRenderer();
         GLTFViewer.loader = new GLTFLoader();
         GLTFViewer.controls = new OrbitControls(GLTFViewer.camera, GLTFViewer.renderer.domElement);
 
         // set the renderer size and add it to the webpage
-        GLTFViewer.renderer.setSize(window.innerWidth - 50, window.innerHeight - 50);
+        GLTFViewer.renderer.setSize(width, height);
         document.body.appendChild(GLTFViewer.renderer.domElement);
+        GLTFViewer.postProcessing();
+        GLTFViewer.composer.setSize(width, height);
 
         // add my own objects to the scene
-        GLTFViewer.camera.position.z = 1;
-        GLTFViewer.addBackground()
+        GLTFViewer.camera.position.z = 100;
+        // GLTFViewer.addBackground()
         GLTFViewer.addLight()
 
         // start the renderer
@@ -51,11 +65,31 @@ export default class GLTFViewer {
      * creates ambient light and a point light at the camera
      */
     static addLight() {
-        GLTFViewer.light = new THREE.PointLight(0xffffff, 0.5);
+        GLTFViewer.light = new THREE.PointLight(0xffffff, 1);
         GLTFViewer.scene.add(GLTFViewer.light);
 
-        const light = new THREE.AmbientLight(0xffffff, 0.1);
+        const light = new THREE.AmbientLight(0xffffff, 0.2);
         GLTFViewer.scene.add(light);
+    }
+
+    /**
+     * anti aliasing
+     */
+    static postProcessing() {
+        // Create the composer and anti aliasing (ssaa) pass
+        GLTFViewer.composer = new EffectComposer(GLTFViewer.renderer);
+        GLTFViewer.composer.setPixelRatio(1); // ensure pixel ratio is always 1 for performance reasons
+        GLTFViewer.ssaaRenderPass = new SSAARenderPass(GLTFViewer.scene, GLTFViewer.camera);
+        GLTFViewer.composer.addPass(GLTFViewer.ssaaRenderPass);
+
+        // set the anti aliasing settings
+        GLTFViewer.ssaaRenderPass.clearColor = 0xffffff; //background color
+        GLTFViewer.ssaaRenderPass.clearAlpha = 0.8; //background opacity/brightness
+
+        GLTFViewer.ssaaRenderPass.sampleLevel = 2; //anti aliasing level (1:1x 2:4x 3:8x etc.)
+        GLTFViewer.ssaaRenderPass.unbiased = true; //unbiased removes weird circles
+        GLTFViewer.ssaaRenderPass.enabled = true; //enable the render pass
+
     }
 
     /**
@@ -91,6 +125,6 @@ export default class GLTFViewer {
         GLTFViewer.light.position.z = GLTFViewer.camera.position.z
 
         // render the scene from the camera
-        GLTFViewer.renderer.render(GLTFViewer.scene, GLTFViewer.camera);
+        GLTFViewer.composer.render();
     }
 }
